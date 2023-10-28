967e02f995b559d31eb19aa4cf5751588f86f217
Simmo Saan <simmo.saan@gmail.com>
2023-10-28 11:18:20 +0300

    Do srand in stubs

--- sv-comp.c
+++ sv-comp.c
@@ -1,4 +1,12 @@
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+// can't srand at the beginning of main, so GCC constructor
+__attribute__((constructor)) static void __sv_sanitizers_srand()  {
+  // getpid to have different seeds for processes created during the same second
+  srand(time(NULL) ^ getpid());
+}
 
 int __VERIFIER_nondet_int() {
   return rand() % 10;
