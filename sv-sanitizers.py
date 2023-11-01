60a0a49104985a9d78cb70ccc9544ce41384da71
Simmo Saan <simmo.saan@gmail.com>
2023-11-01 15:49:24 +0200

    Add trivial violation witness generation

--- sv-sanitizers.py
+++ sv-sanitizers.py
@@ -5,6 +5,8 @@ import subprocess
 from pathlib import Path
 import sys
 import asyncio
+import hashlib
+from datetime import datetime
 
 
 VERSION="0.1.0"
@@ -118,6 +120,83 @@ async def run(args, executable):
         task.cancel()
     return done.pop().result()
 
+def generate_witness(args, result):
+    if args.property == "no-data-race":
+        specification = """CHECK( init(main()), LTL(G ! data-race) )"""
+    elif args.property == "valid-memcleanup":
+        specification = """CHECK( init(main()), LTL(G valid-memcleanup) )"""
+    elif result == "false(valid-deref)":
+        specification = """CHECK( init(main()), LTL(G valid-deref) )"""
+    elif result == "false(valid-free)":
+        specification = """CHECK( init(main()), LTL(G valid-free) )"""
+    elif result == "false(valid-memtrack)":
+        specification = """CHECK( init(main()), LTL(G valid-memtrack) )"""
+    else:
+        raise RuntimeError("unknown witness specification")
+    with open(args.program, "rb") as file:
+        programhash = hashlib.sha256(file.read()).hexdigest()
+    architecture = "32bit" if args.data_model == "ILP32" else "64bit"
+    creationtime = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+    witness = f"""<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <key id="witness-type" for="graph" attr.name="witness-type" attr.type="string"/>
+  <key id="sourcecodelang" for="graph" attr.name="sourcecodelang" attr.type="string"/>
+  <key id="producer" for="graph" attr.name="producer" attr.type="string"/>
+  <key id="specification" for="graph" attr.name="specification" attr.type="string"/>
+  <key id="programfile" for="graph" attr.name="programfile" attr.type="string"/>
+  <key id="programhash" for="graph" attr.name="programhash" attr.type="string"/>
+  <key id="architecture" for="graph" attr.name="architecture" attr.type="string"/>
+  <key id="creationtime" for="graph" attr.name="creationtime" attr.type="string"/>
+  <key id="entry" for="node" attr.name="entry" attr.type="boolean">
+    <default>false</default>
+  </key>
+  <key id="sink" for="node" attr.name="sink" attr.type="boolean">
+    <default>false</default>
+  </key>
+  <key id="violation" for="node" attr.name="violation" attr.type="boolean">
+    <default>false</default>
+  </key>
+  <key id="invariant" for="node" attr.name="invariant" attr.type="string"/>
+  <key id="invariant.scope" for="node" attr.name="invariant.scope" attr.type="string"/>
+  <key id="assumption" for="edge" attr.name="assumption" attr.type="string"/>
+  <key id="assumption.scope" for="edge" attr.name="assumption.scope" attr.type="string"/>
+  <key id="assumption.resultfunction" for="edge" attr.name="assumption.resultfunction" attr.type="string"/>
+  <key id="control" for="edge" attr.name="control" attr.type="string"/>
+  <key id="startline" for="edge" attr.name="startline" attr.type="int"/>
+  <key id="endline" for="edge" attr.name="endline" attr.type="int"/>
+  <key id="startoffset" for="edge" attr.name="startoffset" attr.type="int"/>
+  <key id="endoffset" for="edge" attr.name="endoffset" attr.type="int"/>
+  <key id="enterLoopHead" for="edge" attr.name="enterLoopHead" attr.type="boolean">
+    <default>false</default>
+  </key>
+  <key id="enterFunction" for="edge" attr.name="enterFunction" attr.type="string"/>
+  <key id="returnFromFunction" for="edge" attr.name="returnFromFunction" attr.type="string"/>
+  <key id="threadId" for="edge" attr.name="threadId" attr.type="string"/>
+  <key id="createThread" for="edge" attr.name="createThread" attr.type="string"/>
+  <key id="goblintEdge" for="edge" attr.name="goblintEdge" attr.type="string"/>
+  <key id="goblintLine" for="edge" attr.name="goblintLine" attr.type="string"/>
+  <graph edgedefault="directed">
+    <data key="witness-type">violation_witness</data>
+    <data key="sourcecodelang">C</data>
+    <data key="producer">sv-sanitizers {VERSION}</data>
+    <data key="specification">{specification}</data>
+    <data key="programfile">{args.program}</data>
+    <data key="programhash">{programhash}</data>
+    <data key="architecture">{architecture}</data>
+    <data key="creationtime">{creationtime}</data>
+    <node id="N0">
+      <data key="entry">true</data>
+    </node>
+    <node id="N1">
+      <data key="violation">true</data>
+    </node>
+    <edge id="E0" source="N0" target="N1"/>
+  </graph>
+</graphml>
+"""
+    with open("witness.graphml", "w") as file:
+        file.write(witness)
+
 async def main():
     args = parse_args()
     args.property = parse_property(args.property)
@@ -127,5 +206,6 @@ async def main():
     sys.stderr.buffer.write(output)
     sys.stderr.flush()
     print(f"SV-COMP result: {result}")
+    generate_witness(args, result)
 
 asyncio.run(main())
